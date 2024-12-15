30f185f6312189e7992ebacded5fcde6e4ae386e
Anthony Salgado <anthonyasalgado@gmail.com>
2024-12-15 11:23:33 -0800

    added delete factory function

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,14 +2,17 @@ const express = require('express');
 const {
   getAllReviews,
   createReview,
+  deleteReview,
 } = require('../controllers/reviewController');
 const { protect, restrictTo } = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
   .get(getAllReviews)
   .post(protect, restrictTo('user'), createReview);
+
+router.route('/:id').delete(deleteReview);
 
 module.exports = router;
