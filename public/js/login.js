63d564d7a5fb1fd71bf821c2ea6b9bf947364ef5
Anthony Salgado <anthonyasalgado@gmail.com>
2025-01-09 01:56:01 -0800

    still working on login

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,25 @@
 /* eslint-disable */
 
 const login = async (email, password) => {
-  const res = await axios({
-    method: 'POST',
-    url: 'http://127.0.0.1:3000/api/v1/users/login',
-    data: {
-      email,
-      password,
-    },
-  });
-  console.log(res);
+  console.log(email, password);
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+    if (res.data.status === 'success') {
+      alert('Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    alert(err.response.data.message);
+  }
 };
 
 document.querySelector('.form').addEventListener('submit', (e) => {
