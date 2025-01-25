874e6c2c2d29467d097c82c5d4b1e20b1f65dfb4
Anthony Salgado <anthonyasalgado@gmail.com>
2025-01-25 01:55:27 -0800

    trying to fix parcel

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
+const sanitizeHtml = require('sanitize-html');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 
@@ -87,7 +87,10 @@ app.use(cookieParser());
 app.use(mongoSanitize());
 
 // Data sanitization against XSS
-app.use(xss());
+app.use((req, res, next) => {
+  req.body = sanitizeHtml(req.body);
+  next();
+});
 
 // Prevent parameter pollution
 app.use(
