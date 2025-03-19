defb13f68916bb9eb1f9da844dfeede55b22090a
Anthony Salgado <anthonyasalgado@gmail.com>
2025-03-19 12:19:16 -0700

    createbookingcheckout without logs

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -42,27 +42,27 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-// const createBookingCheckout = async (session) => {
-//   const tour = session.client_reference_id;
-//   const user = (await User.findOne({ email: session.customer_email })).id;
-//   const price = session.display_items[0].amount / 100;
-//   await Booking.create({ tour, user, price });
-// };
-
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.amount_total / 100;
-
-  console.log('Creating booking:', { tour, user, price });
-
-  try {
-    const booking = await Booking.create({ tour, user, price });
-    console.log('Booking created successfully:', booking);
-  } catch (err) {
-    console.log('Error creating booking:', err);
-  }
+  const price = session.display_items[0].amount / 100;
+  await Booking.create({ tour, user, price });
 };
+
+// const createBookingCheckout = async (session) => {
+//   const tour = session.client_reference_id;
+//   const user = (await User.findOne({ email: session.customer_email })).id;
+//   const price = session.amount_total / 100;
+
+//   console.log('Creating booking:', { tour, user, price });
+
+//   try {
+//     const booking = await Booking.create({ tour, user, price });
+//     console.log('Booking created successfully:', booking);
+//   } catch (err) {
+//     console.log('Error creating booking:', err);
+//   }
+// };
 
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
