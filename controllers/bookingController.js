d8545f4d0244bf400c4e8c18c866fcb227447aed
Anthony Salgado <anthonyasalgado@gmail.com>
2025-03-19 14:33:57 -0700

    debugging session obj

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -43,9 +43,15 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const createBookingCheckout = async (session) => {
+  console.log('session', session.display_items);
+  console.log('session', session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price =
+    session.display_items && session.display_items.length > 0
+      ? session.display_items[0].amount / 100
+      : 0;
+  // const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
